1b133cde8b7bf793f067bbf44a8fe100e25e3151
splitti <45425109+splitti@users.noreply.github.com>
2026-09-19 17:42:14 +0200

    fix: resolve initial Android CI compile issues

--- app/src/main/java/de/mupibox/control/ui/boxes/BoxesScreen.kt
+++ app/src/main/java/de/mupibox/control/ui/boxes/BoxesScreen.kt
@@ -14,7 +14,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Speaker
-import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api\nimport androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
